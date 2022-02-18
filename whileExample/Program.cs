eba78e0ad14cdca90b64698968d9297fde3f363e
bekiricli <82258691+Hitikiri@users.noreply.github.com>
2022-02-18 20:36:20 +0300

    even number sum
    for

---
+++ whileExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace whileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //x den y ye kadar olan çift sayıların toplamını bul
+            int x, y,z;
+            z = 0;
+            Console.WriteLine("ilk sayıyı giriniz");
+            x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ikinci sayıyı giriniz");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i < y; i++)
+            {
+                if (i%2==0)
+                {
+                    z += i;
+                }
+            }   Console.WriteLine("toplam:"+z);
+        }   
+    }
+}
